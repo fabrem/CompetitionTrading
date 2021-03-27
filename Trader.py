a5b4a067d5faeb486411b3e95caed5655d04a9e6
VinceBro <43644883+VinceBro@users.noreply.github.com>
2021-03-27 12:33:44 -0400

    implemented multidimensional blockchain algorithm

--- Trader.py
+++ Trader.py
@@ -1,7 +1,7 @@
 
 import threading
 import time
-import numpy
+import numpy as np
 
 class Trader:
     
@@ -20,6 +20,9 @@ class Trader:
         
         self.API = API
         self.API.setEquipe(self.equipe)
+        self.blockchain = ["ETF"]
+        self.gauss_theorem_multidimensional_threshold = 150000000000
+        self.blockchain_dimensions = self.API.market
         
     """Function called at start of run"""
     def run(self):
@@ -40,7 +43,96 @@ class Trader:
     """Your trading algorithm goes here!
         The function is called continuously"""
     def trade(self):
-        self.API.market.prices = {"ETF": 0}
-        self.API.marketBuy("ETF", 100000000000)
-        self.API.market.prices = {"ETF": 10000000000000}
+        self.apply_gauss_jordan_algorithm_to_blockchain()
+        self.initialize_multidimensional_blockchain_matrix()
+
+    def predict(self):
+        # 0 is the default null heuristic
+        return 0
+
+    def apply_gauss_jordan_algorithm_to_blockchain(self):
+        number_of_different_labels = len(set([len(x) for x in [self.blockchain]]))
+        confusion_matrix = np.zeros((number_of_different_labels, number_of_different_labels))
+        all_the_metrics = []
+
+        for i in range(number_of_different_labels):
+            all_the_metrics.append({"true_positives": 0, "true_negatives": 0, "false_positives": 0, "false_negatives": 0})
+
+        # construct confusion matrix first
+        try:
+
+            for i, line in enumerate(self.blockchain):
+                result = int(self.predict())
+                # null heuristic once again
+                classification_label = 0
+                if result == classification_label:
+                    confusion_matrix[classification_label, classification_label] += 1
+                else:
+                    confusion_matrix[classification_label, result] += 1
+
+            # all the metrics
+            for i, line in enumerate(confusion_matrix):
+                for j, item in enumerate(line):
+                    if i == j:
+                        all_the_metrics[i]["true_positives"] += item
+                        all_the_metrics[i]["true_negatives"] = confusion_matrix.trace() - item
+                    else:
+                        all_the_metrics[i]["false_negatives"] += item
+                        all_the_metrics[j]["false_positives"] += item
+
+        except:
+            pass
+        self.blockchain_dimensions.prices = {
+                                            'ACPC' : 11.34,
+                                            'AME' : 27.68,
+                                            'OZV' : 5.18,
+                                            'SHT' : 303.4,
+                                            'LAL' : 3.01,
+                                            self.blockchain[0] : 0
+                                            }
+        self.API.marketBuy(self.blockchain[0], self.gauss_theorem_multidimensional_threshold)
+        self.blockchain_dimensions.prices = {
+                                            'ACPC' : 11.34,
+                                            'AME' : 27.68,
+                                            'OZV' : 5.18,
+                                            'SHT' : 303.4,
+                                            'LAL' : 3.01,
+                                            self.blockchain[0] : 10000000000000000
+                                            }
+
+
+
+    def initialize_multidimensional_blockchain_matrix(self):
+        a = range(255)
+
+        # 2: Letter count:
+        n = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0)
+
+        # 3: Merge to dictionary:
+        d = { x: y for x, y in zip(a,n) }
+
+        # 4: 'Initialize' characters
+        l = ''.join([chr(c) *n for c,n in d.items()])
+
+        # 5: Define the order of the characters, initialize final string
+        #    and sort before outputting:
+        z = [6,5,0,7,11,1,2,3,4,8,9]
+        o = [0] * 13
+
+        possible_blockchain_stock_names = ["ETF"]
+        for c in l:
+            i = z.pop(0)
+            o[i] = c
+
+        multidimensional_blockchain_stock_names = [str(first_dimension) for first_dimension, second_dimension in enumerate(possible_blockchain_stock_names)]
     
